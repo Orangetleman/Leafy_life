810a80093da324950041a53b1549c03244662bac
Orangentleman <christophe33.de.almeida@gmail.com>
2025-12-18 14:59:54 +0100

    apellation changé

--- js/data/leafs.js
+++ js/data/leafs.js
@@ -1,3 +1,3 @@
 export const LEAFS = [
-    { id: 0, name: "pissenlit", type : "attaquant", rarity: "default", atk: 5, hp: 4, species: "plante", biome: 1, lvl: 1 }
-]
+    { id: 1, name: "pissenlit", type : "attaquant", rarity: "default", atk: 5, hp: 4, species: "plante", biome: 1, lvl: 1 }
+];
