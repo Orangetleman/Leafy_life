a9855c1ff640289c8418bd689cf85cb43a6e60f2
Orangentleman <christophe33.de.almeida@gmail.com>
2025-12-22 23:29:47 +0100

    rien de bien fou

--- js/data/planets.js
+++ js/data/planets.js
@@ -1,3 +1,8 @@
+let plain = "plain";
+let forest = "forest";
+let lake = "lake";
+let mountain = "mountain";
+
 export const PLANETS = [
-    { id: 1, biomes: [plain, forest, lake, montain] }
+    { id: 1, biomes: [plain, forest, lake, mountain] }
 ];
--- js/screens/inventory/inventoryHome.js
+++ js/screens/inventory/inventoryHome.js
@@ -3,5 +3,14 @@ import { ITEMS } from "../../data/items.js";
 export default class InventoryHome {
     render() {
         console.log("Inventory:", ITEMS);
+        const game = document.getElementById("game");
+        game.innerHTML = `
+            <div class="inventory">
+                <h2>Inventory</h2>
+                <ul>
+                    ${ITEMS.map(item => `<li>${item.name}</li>`).join('')}
+                </ul>
+            </div>
+        `;
     }
 }
