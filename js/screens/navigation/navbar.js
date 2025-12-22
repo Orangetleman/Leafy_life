d521670961c6b9016290a1cb1b563467e2381061
Blackyeve <blackthunderp4@yahoo.com>
2025-12-22 15:36:15 +0100

    Delete navbar.js

--- js/screens/navigation/navbar.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "../../router.js";
-
-export function setupNavbar() {
-    document.getElementById("btnLeafs").onclick = () => Router.navigate("leafs/leafsHome");
-    document.getElementById("btnShop").onclick = () => Router.navigate("shop/shopHome");
-    document.getElementById("btnInventory").onclick = () => Router.navigate("inventory/inventoryHome");
-    document.getElementById("btnPlanet").onclick = () => Router.navigate("planet/planetHome");
-}
